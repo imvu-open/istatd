ca445c93b5e9d066a61260bbb9b874f8d71a97a3
Jon Watte <jwatte@imvu.com>
2012-09-27 16:58:04 -0700

    debug quickstart

--- quickstart.sh
+++ quickstart.sh
@@ -4,7 +4,7 @@ set -o nounset
 
 if [ -x /usr/bin/python2 ]; then
     echo "Adding symlink to python2 into PATH."
-    ln -s /usr/bin/python2 `pwd`/python
+    ln -sf /usr/bin/python2 `pwd`/python
     export PATH=`pwd`":$PATH"
 fi
 
@@ -14,6 +14,8 @@ echo "calling make -j4"
 make -j4
 echo "setting up in /var/tmp"
 mkdir /var/tmp/istatd-quickstart
-ln -sf `pwd`/files /var/tmp/files
+mkdir /var/tmp/istatd-quickstart/store
+mkdir /var/tmp/istatd-quickstart/settings
+ln -sf `pwd`/files /var/tmp/istatd-quickstart/files
 echo "istatd starting up -- hit localhost:18011 in a browser to see it in action"
-bin/istatd --settings quickstart.cfg --user `logname`
+bin/istatd --config quickstart.cfg --user `logname`
