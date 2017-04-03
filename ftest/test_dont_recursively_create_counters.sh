e6616f5f99df1dbc3d4d6a9e870769b5fb47e649
Leonidas Loucas <lloucas@imvu.com>
2017-04-03 16:04:20 -0700

    Add test to ensure that istatd with dont recursively create counters will not recreate intermediate counters on restart

--- ftest/test_dont_recursively_create_counters.sh
+++ ftest/test_dont_recursively_create_counters.sh
@@ -15,5 +15,16 @@ check_counters 18011 'foo.bar.*' 'foo.bar.quux' 'foo.bar.baz'
 test_counter_does_not_exist no-recurse-single 'foo.10s'
 test_counter_does_not_exist no-recurse-single 'foo.bar.10s'
 
+kill_server no-recurse-single
+
+echo "Restarting server to verify counters dont come back"
+# Verify that the intermediate counters are not recreated on start
+start_server no-recurse-single
+
+wait_for_file $DBDIR/no-recurse-single-store/foo/bar/quux/10s
+
+test_counter_does_not_exist no-recurse-single 'foo.10s'
+test_counter_does_not_exist no-recurse-single 'foo.bar.10s'
+
 cleanup_test
 
