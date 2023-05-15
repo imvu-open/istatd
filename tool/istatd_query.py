b781fc0d7e527737c925c89c96cbd604e63ebe8f
Eric Hohenstein <ehohenstein@imvu.com>
2023-05-15 15:35:17 +0000

    use python3 instead of python2 for tests since python2 has been dead for a decade

--- tool/istatd_query.py
+++ tool/istatd_query.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import sys
-import urllib
-import urlparse
+import urllib.request, urllib.parse, urllib.error
+import urllib.parse
 import json
 import pprint
 
@@ -17,16 +17,16 @@ def urlencode_params(start_time, end_time, max_samples):
     if max_samples != "null" and max_samples != 0:
         params["samples"] = max_samples
 
-    return urllib.urlencode(params)
+    return urllib.parse.urlencode(params)
     
 def fetch_counter_via_GET(istatd_host, counter, start_time=0, end_time=0, max_samples=0):
     params = urlencode_params(start_time, end_time, max_samples)
 
-    url = urlparse.urljoin('http://' + istatd_host, '/%s?%s' % (counter, params))
-    print 'GET', url
+    url = urllib.parse.urljoin('http://' + istatd_host, '/%s?%s' % (counter, params))
+    print('GET', url)
 
     try:
-        result = urllib.urlopen(url)
+        result = urllib.request.urlopen(url)
     except IOError as ioerror:
         return {"error" : ioerror}
     except: 
@@ -41,11 +41,11 @@ def fetch_counters_via_POST(istatd_host, counters, start_time=0, end_time=0, max
     request["keys"] = counters
     request["maxSamples"] = 0 if max_samples == "null" else int(max_samples)
     request_str = json.dumps(request)
-    url = urlparse.urljoin('http://' + istatd_host, '/*')
-    print 'POST', url, request_str
+    url = urllib.parse.urljoin('http://' + istatd_host, '/*')
+    print('POST', url, request_str)
 
     try:
-        result = urllib.urlopen(url, request_str)
+        result = urllib.request.urlopen(url, request_str.encode('utf-8'))
     except IOError as ioerror:
         return {"error" : ioerror}
     except: 
@@ -67,12 +67,6 @@ def __main(argv):
     if not options.host:
         parser.error("--host required")
 
-    if options.start_time != "null" and options.start_time < 0:
-        parser.error("invalid start time %s" % options.start_time)
-
-    if options.end_time != "null" and options.end_time < 0:
-        parser.error("invalid end time %s" % options.end_time)
-
     counters = args[1:]
     if len(counters) == 1 and not options.force_post:
         result = fetch_counter_via_GET(options.host, counters[0], options.start_time, options.end_time, options.max_samples)
@@ -83,48 +77,49 @@ def __main(argv):
         pprint.pprint(result)
     else:
         if "error" in result:
-            print "error: {0}".format(result["error"])
+            print("error: {0}".format(result["error"]))
         elif "buckets" in result:
             if "start" in result:
-                print "start:    {0:10}".format(result["start"])
+                print("start:    {0:10}".format(result["start"]))
 
             if "end" in result:
-                print "end:      {0:10}".format(result["end"])
+                print("end:      {0:10}".format(result["end"]))
             
             if "interval" in result:
-                print "interval: {0:10}".format(result["interval"])
+                print("interval: {0:10}".format(result["interval"]))
 
             for b in result["buckets"]:
                 d = b["data"]
                 if "avg" in d:
-                    print "{0:10} {1:.2f} {2:.2f} {3:.2f} {4:d} {5:.2f} {6:.2f}".format(b["time"], d["avg"], d["min"], d["max"], d["count"], d["sum"], d["sumsq"])
+                    print("{0:10} {1:.2f} {2:.2f} {3:.2f} {4:d} {5:.2f} {6:.2f}".format(b["time"], d["avg"], d["min"], d["max"], d["count"], d["sum"], d["sumsq"]))
                 else:
-                    print "{0:10} empty bucket".format(b["time"])
+                    print("{0:10} empty bucket".format(b["time"]))
         else:
             if "start" in result:
-                print "start:    {0:10}".format(result["start"])
+                print("start:    {0:10}".format(result["start"]))
 
             if "stop" in result:
-                print "stop:     {0:10}".format(result["stop"])
+                print("stop:     {0:10}".format(result["stop"]))
             
             if "interval" in result:
-                print "interval: {0:10}".format(result["interval"])
+                print("interval: {0:10}".format(result["interval"]))
 
-            for k,v in result.iteritems():
+            for k in sorted(result.keys()):
+                v = result[k]
                 if k not in ["start", "stop", "interval"]:
-                    print "counter:", k
+                    print("counter:", k)
                     if v == 'Not found.':
-                        print "  Not found."
+                        print("  Not found.")
                     else:
-                        print "  start:    {0:6}".format(v["start"])
-                        print "  end:      {0:6}".format(v["end"])
-                        print "  interval: {0:6}".format(v["interval"])
-                        print "  data:"
+                        print("  start:    {0:6}".format(v["start"]))
+                        print("  end:      {0:6}".format(v["end"]))
+                        print("  interval: {0:6}".format(v["interval"]))
+                        print("  data:")
                         for d in v["data"]:
                             if "avg" in d:
-                                print "{0:10} {1:.2f} {2:.2f} {3:.2f} {4:d} {5:.2f} {6:.2f}".format(d["time"], d["avg"], d["min"], d["max"], d["count"], d["sum"], d["sumsq"])
+                                print("{0:10} {1:.2f} {2:.2f} {3:.2f} {4:d} {5:.2f} {6:.2f}".format(d["time"], d["avg"], d["min"], d["max"], d["count"], d["sum"], d["sumsq"]))
                             else:
-                                print "{0:10} empty bucket".format(b["time"])
+                                print("{0:10} empty bucket".format(b["time"]))
 
     return 0
 
